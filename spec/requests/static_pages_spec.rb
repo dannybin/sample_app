35d224fdcb7fd978fd8f63833624659490aa1884
danny <bin.danny@gmail.com>
2014-03-12 01:07:53 -0700

    Finish layout and routes

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  subject { page }
+
+  describe "Home Page" do
+    before { visit root_path }
+
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+
+  end
+
+  describe "Help Page" do
+    before { visit help_path }
+
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
+  end
+
+  describe "About Page" do
+    before { visit about_path }
+
+    it { should have_content('About Us') }
+    it { should have_title(full_title('About Us')) }
+  end
+
+  describe "Contact" do
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
+  end
+end
